b3bdf3bda2ff567700195ebf27e5a10d64481658
Samuel Prak <samuel@samuelprak.fr>
2022-02-04 22:04:35 +0100

    fixtures

---
+++ src/scripts/seed-database.ts
@@ -0,0 +1,51 @@
+import * as path from "path"
+import { program } from "commander"
+import glob from "glob-promise"
+import ormconfig from "ormconfig"
+import { Connection, createConnection, getRepository } from "typeorm"
+import {
+  Builder,
+  fixturesIterator,
+  Loader,
+  Parser,
+  Resolver,
+} from "typeorm-fixtures-cli/dist"
+
+program.option("--drop")
+program.parse()
+
+const loadFixtures = async (connection: Connection, fixturesPath: string) => {
+  try {
+    const loader = new Loader()
+    loader.load(path.resolve(fixturesPath))
+
+    const resolver = new Resolver()
+    const fixtures = resolver.resolve(loader.fixtureConfigs)
+    const builder = new Builder(connection, new Parser())
+
+    for (const fixture of fixturesIterator(fixtures)) {
+      const entity = await builder.build(fixture)
+      await getRepository(entity.constructor.name).save(entity)
+    }
+  } catch (err) {
+    throw err
+  } finally {
+    if (connection) {
+      await connection.close()
+    }
+  }
+}
+
+const main = async () => {
+  const options = program.opts<{ drop: boolean }>()
+
+  const connection = await createConnection(ormconfig)
+  await connection.synchronize(options.drop)
+
+  const files = await glob("./**/fixtures/*.yml")
+  for (const file of files) {
+    loadFixtures(connection, file)
+  }
+}
+
+main()
