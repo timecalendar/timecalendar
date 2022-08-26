ab4e8dd56b82e7b3fca5abd7a25a28079d23ca9e
Samuel Prak <samuel@samuelprak.fr>
2022-08-26 14:55:02 +0200

    web

---
+++ web/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "styles/globals.css"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import type { AppProps } from "next/app"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
