1cb0e1872b4ec31bcf92788409490117401a4e5e
Samuel Prak <samuel@samuelprak.fr>
2023-08-31 23:55:07 +0200

    deploy on production

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -4,10 +4,8 @@ if [ -z "$ENVIRONMENT" ]; then
   if [ ! -z "$GITHUB_REF" ]; then
     if [[ $GITHUB_REF == 'refs/heads/main' ]] || [[ $GITHUB_REF == 'refs/heads/github-cd' ]]; then
       ENVIRONMENT="preprod"
-      KUBE_NAMESPACE="timecalendar-preprod"
     elif [[ $GITHUB_REF == 'refs/heads/production' ]]; then
       ENVIRONMENT="production"
-      KUBE_NAMESPACE="timecalendar-production"
     else
       printf '%s\n' "Cannot deploy from the branch $GITHUB_REF" >&2
       exit 1
@@ -18,13 +16,30 @@ if [ -z "$ENVIRONMENT" ]; then
   fi
 fi
 
+if [ -z "$DOCKER_TAG" ]; then
+  printf '%s\n' "Missing DOCKER_TAG variable" >&2
+  exit 1
+fi
+
 KUBE_NAMESPACE="timecalendar-$ENVIRONMENT"
+
+echo Environment to deploy: $ENVIRONMENT
+echo Docker tag to deploy: $DOCKER_TAG
+echo "-----------------------------------"
+echo Namespace: $KUBE_NAMESPACE
 
 CURRENT_PATH="`dirname \"$0\"`"
 ENVIRONMENT_PATH=$CURRENT_PATH/environments/$ENVIRONMENT
 
 kubectl apply -f $ENVIRONMENT_PATH/sealedsecret.yaml
-helm upgrade -f $ENVIRONMENT_PATH/values.yaml -f $ENVIRONMENT_PATH/secret-values.yaml timecalendar $CURRENT_PATH/timecalendar -n $KUBE_NAMESPACE --render-subchart-notes || exit 1
+helm upgrade -f $ENVIRONMENT_PATH/values.yaml \
+  -f $ENVIRONMENT_PATH/secret-values.yaml \
+  --set server.tag=$DOCKER_TAG \
+  --set web.tag=$DOCKER_TAG \
+  timecalendar $CURRENT_PATH/timecalendar -n $KUBE_NAMESPACE --render-subchart-notes || exit 1
 
 kubectl rollout restart -n $KUBE_NAMESPACE deployment timecalendar
 kubectl rollout status -n $KUBE_NAMESPACE deployment timecalendar
+
+kubectl rollout restart -n $KUBE_NAMESPACE deployment timecalendar-web
+kubectl rollout status -n $KUBE_NAMESPACE deployment timecalendar-web
