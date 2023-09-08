ffe4a0a432415a58a71f4364bd0b2605a33ebd22
Samuel Prak <samuel@samuelprak.fr>
2023-09-08 23:57:28 +0200

    parse from description + start web page

--- server/src/modules/fetch/schools/upec/upec-strategy.ts
+++ server/src/modules/fetch/schools/upec/upec-strategy.ts
@@ -1,9 +1,9 @@
-import ParseTeachersPipe from "modules/fetch/pipes/parse-teachers-pipe"
+import parseTeachersPipe from "modules/fetch/pipes/parse-teachers-pipe"
 import { SchoolStrategy } from "modules/fetch/strategies/school-strategy"
 
 const upecStrategy = new SchoolStrategy({
   school: "upec",
-  eventPipes: [ParseTeachersPipe(/^([A-Z-]{2,} )+[A-Z-]{2,}$/)],
+  eventPipes: [parseTeachersPipe(/^([A-Z-]{2,} )+[A-Z-]{2,}$/)],
 })
 
 export default upecStrategy
