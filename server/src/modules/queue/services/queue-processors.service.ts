ec81b66d7ac2c83be1c32d7f9f5684b357e025f8
Samuel Prak <samuel@samuelprak.fr>
2025-01-19 21:10:31 +0100

    chore: add queue concurrency

--- server/src/modules/queue/services/queue-processors.service.ts
+++ server/src/modules/queue/services/queue-processors.service.ts
@@ -1,11 +1,14 @@
 import { Processor, WorkerHost } from "@nestjs/bullmq"
 import { Injectable } from "@nestjs/common"
 import { Job } from "bullmq"
+import { QUEUE_CONCURRENCY } from "config/constants"
 import { DEFAULT_QUEUE_NAME } from "modules/queue/queue.constants"
 import { QueueService } from "modules/queue/services/queue.service"
 
 @Injectable()
-@Processor(DEFAULT_QUEUE_NAME)
+@Processor(DEFAULT_QUEUE_NAME, {
+  concurrency: QUEUE_CONCURRENCY,
+})
 export class DefaultQueueProcessorService extends WorkerHost {
   constructor(private readonly queueService: QueueService) {
     super()
