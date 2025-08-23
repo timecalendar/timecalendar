cb28d3a6fbe4b23626bfba023c726ec18620ce51
Samuel Prak <samuel@samuelprak.fr>
2025-08-23 14:39:32 +0200

    fix android build

--- app/lib/modules/hidden_event/screens/hidden_events_screen.dart
+++ app/lib/modules/hidden_event/screens/hidden_events_screen.dart
@@ -4,10 +4,63 @@ import 'package:timecalendar/modules/calendar/providers/events_provider.dart';
 import 'package:timecalendar/modules/hidden_event/providers/hidden_event_provider.dart';
 import 'package:timecalendar/modules/hidden_event/widgets/hidden_event_item.dart';
 
+class _HiddenEventDisplayItem {
+  final String? namedEvent;
+  final dynamic event;
+  final int originalNamedIndex;
+  final int originalUidIndex;
+  final bool isNamedEvent;
+
+  _HiddenEventDisplayItem.named({
+    required this.namedEvent,
+    required this.originalNamedIndex,
+  }) : event = null,
+       originalUidIndex = -1,
+       isNamedEvent = true;
+
+  _HiddenEventDisplayItem.uid({
+    required this.event,
+    required this.originalUidIndex,
+  }) : namedEvent = null,
+       originalNamedIndex = -1,
+       isNamedEvent = false;
+}
+
 class HiddenEventsScreen extends ConsumerWidget {
   static const routeName = '/hidden_events';
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  List<_HiddenEventDisplayItem> _buildDisplayItems(
+    List<String> namedHiddenEvents,
+    List<String> uidHiddenEvents,
+    List<dynamic> events,
+  ) {
+    final displayItems = <_HiddenEventDisplayItem>[];
+
+    // Add all named events
+    for (int i = 0; i < namedHiddenEvents.length; i++) {
+      displayItems.add(
+        _HiddenEventDisplayItem.named(
+          namedEvent: namedHiddenEvents[i],
+          originalNamedIndex: i,
+        ),
+      );
+    }
+
+    // Add only existing uid events
+    for (int i = 0; i < uidHiddenEvents.length; i++) {
+      final targetUid = uidHiddenEvents[i];
+      final event = events.where((event) => event.uid == targetUid).firstOrNull;
+      if (event != null) {
+        displayItems.add(
+          _HiddenEventDisplayItem.uid(event: event, originalUidIndex: i),
+        );
+      }
+    }
+
+    return displayItems;
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -18,55 +71,46 @@ class HiddenEventsScreen extends ConsumerWidget {
 
     return eventsAsync.when(
       data: (events) {
+        final displayItems = _buildDisplayItems(
+          namedHiddenEvents,
+          uidHiddenEvents,
+          events,
+        );
+
         Widget _itemBuilder(BuildContext context, int index) {
-          return (index < namedHiddenEvents.length)
-              ? HiddenEventItem(
-                namedEvent: namedHiddenEvents[index],
-                index: index,
-                removeItem:
-                    () => ref
-                        .read(hiddenEventProvider.notifier)
-                        .removeNamedEventByIndex(index),
-              )
-              : HiddenEventItem(
-                event: events.firstWhere(
-                  (event) =>
-                      event.uid ==
-                      uidHiddenEvents[index - namedHiddenEvents.length],
-                ),
-                index: index,
-                removeItem:
-                    () => ref
-                        .read(hiddenEventProvider.notifier)
-                        .removeUidEventByIndex(
-                          index - namedHiddenEvents.length,
-                        ),
-              );
+          final item = displayItems[index];
+
+          if (item.isNamedEvent) {
+            return HiddenEventItem(
+              namedEvent: item.namedEvent!,
+              index: index,
+              removeItem:
+                  () => ref
+                      .read(hiddenEventProvider.notifier)
+                      .removeNamedEventByIndex(item.originalNamedIndex),
+            );
+          } else {
+            return HiddenEventItem(
+              event: item.event,
+              index: index,
+              removeItem:
+                  () => ref
+                      .read(hiddenEventProvider.notifier)
+                      .removeUidEventByIndex(item.originalUidIndex),
+            );
+          }
         }
 
-        return _buildScaffold(_itemBuilder, namedHiddenEvents, uidHiddenEvents);
+        return _buildScaffold(_itemBuilder, displayItems);
       },
-      loading:
-          () => _buildScaffold(
-            null,
-            namedHiddenEvents,
-            uidHiddenEvents,
-            isLoading: true,
-          ),
-      error:
-          (error, stack) => _buildScaffold(
-            null,
-            namedHiddenEvents,
-            uidHiddenEvents,
-            error: error,
-          ),
+      loading: () => _buildScaffold(null, [], isLoading: true),
+      error: (error, stack) => _buildScaffold(null, [], error: error),
     );
   }
 
   Widget _buildScaffold(
     Widget Function(BuildContext, int)? itemBuilder,
-    List<String> namedHiddenEvents,
-    List<String> uidHiddenEvents, {
+    List<_HiddenEventDisplayItem> displayItems, {
     bool isLoading = false,
     Object? error,
   }) {
@@ -75,8 +119,7 @@ class HiddenEventsScreen extends ConsumerWidget {
       appBar: AppBar(title: Text('Événements masqués')),
       body: _buildBody(
         itemBuilder,
-        namedHiddenEvents,
-        uidHiddenEvents,
+        displayItems,
         isLoading: isLoading,
         error: error,
       ),
@@ -85,8 +128,7 @@ class HiddenEventsScreen extends ConsumerWidget {
 
   Widget _buildBody(
     Widget Function(BuildContext, int)? itemBuilder,
-    List<String> namedHiddenEvents,
-    List<String> uidHiddenEvents, {
+    List<_HiddenEventDisplayItem> displayItems, {
     bool isLoading = false,
     Object? error,
   }) {
@@ -98,11 +140,10 @@ class HiddenEventsScreen extends ConsumerWidget {
       return Center(child: Text('Erreur: $error'));
     }
 
-    if (namedHiddenEvents.length + uidHiddenEvents.length > 0 &&
-        itemBuilder != null) {
+    if (displayItems.isNotEmpty && itemBuilder != null) {
       return ListView.builder(
         itemBuilder: itemBuilder,
-        itemCount: namedHiddenEvents.length + uidHiddenEvents.length,
+        itemCount: displayItems.length,
       );
     }
 
